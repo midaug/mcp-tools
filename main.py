1ec6c4f4b8dceeb54b21c06ce47c5ea2272a07fa
huangxianjun <huangxianjun@qianli-ai.com>
2025-11-24 14:10:33 +0800

    fix:Fix spelling error in main.py and update dependency version
    - 修复 `main.py` 中的拼写错误，并将 `time_tool.get_complete_calendar_info()` 改为 `time_tools.get_complete_calendar_info()`。
    - 更新依赖版本为 `fastmcp>=2.13.1`。
    - 提高fastmcp版本到2.13.1以确保兼容性。

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ mcp = FastMCP("MD的工具箱")
 def get_current_time() -> str:
     """获取当前时间格式化后的的日期时间字符串，格式：yyyy年MM月dd日 HH点mm分ss秒 星期，农历"""
     try:
-        info = time_tool.get_complete_calendar_info()
+        info = time_tools.get_complete_calendar_info()
         return f"今天日期信息为: {str(info["complete_display"])}"
     except Exception as e:
         return f"工具执行错误: {str(e)}"
@@ -20,10 +20,10 @@ if __name__ == "__main__":
     # 启动服务器（支持多种传输方式）
     
     # 方式1: STDIO（适合Claude Desktop等客户端）
-    # mcp.run(transport="stdio")
+    mcp.run(transport="stdio")
     
     # 方式2: HTTP（适合Web客户端）
     # mcp.run(transport="http", host="0.0.0.0", port=8000)
     
     # 方式3: SSE（Server-Sent Events）
-    mcp.run(transport="sse", host="0.0.0.0", port=8000)
+    # mcp.run(transport="sse", host="0.0.0.0", port=8000)
